b9cf3e0ce100001024ac037c6f61858c23624e92
PachchigarHetal <114840001+PachchigarHetal@users.noreply.github.com>
2022-10-30 21:52:48 -0400

    Add files via upload

---
+++ doctors/src/model/details.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author hetalpachchigar
+ */
+public class details {
+  
+    private string location;
+    private string finddoctor;
+    private string requestappointment;
+    private string hospitaldirectory;
+    private string persondirectory;
+    private string vitalsigns;
+
+    public string getVitalsigns() {
+        return vitalsigns;
+    }
+
+    public void setVitalsigns(string vitalsigns) {
+        this.vitalsigns = vitalsigns;
+    }
+    
+
+    public string getHospitaldirectory() {
+        return hospitaldirectory;
+    }
+
+    public void setHospitaldirectory(string hospitaldirectory) {
+        this.hospitaldirectory = hospitaldirectory;
+    }
+
+    public string getPersondirectory() {
+        return persondirectory;
+    }
+
+    public void setPersondirectory(string persondirectory) {
+        this.persondirectory = persondirectory;
+    }
+    
+
+
+    public string getLocation() {
+        return location;
+    }
+
+    public void setLocation(string location) {
+        this.location = location;
+    }
+
+    public string getFinddoctor() {
+        return finddoctor;
+    }
+
+    public void setFinddoctor(string finddoctor) {
+        this.finddoctor = finddoctor;
+    }
+
+    public string getRequestappointment() {
+        return requestappointment;
+    }
+
+    public void setRequestappointment(string requestappointment) {
+        this.requestappointment = requestappointment;
+    }
+    
+    
+  
+}
